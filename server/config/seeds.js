b1ba5d868f9b1b6658cf4566297e86ff3ef8804c
MiaLaFleur <mia.lafleur@yahoo.com>
2022-08-02 19:38:15 -0500

    changed item descriptions

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,153 +1,147 @@
-const db = require('./connection');
-const { User, Product, Category } = require('../models');
+const db = require("./connection");
+const { User, Product, Category } = require("../models");
 
-db.once('open', async () => {
+db.once("open", async () => {
   await Category.deleteMany();
 
   const categories = await Category.insertMany([
-    { name: 'Food' },
-    { name: 'Household Supplies' },
-    { name: 'Electronics' },
-    { name: 'Books' },
-    { name: 'Toys' }
+    { name: "Food" },
+    { name: "Household Supplies" },
+    { name: "Electronics" },
+    { name: "Books" },
+    { name: "Toys" },
   ]);
 
-  console.log('categories seeded');
+  console.log("categories seeded");
 
   await Product.deleteMany();
 
   const products = await Product.insertMany([
     {
-      name: 'Box of Cookies',
-      description:
-        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      image: 'cookies.jpg',
+      name: "Box of Cookies",
+      description: "A box with a dozen chocolate chip cookies inside.",
+      image: "cookies.jpg",
       category: categories[0]._id,
       price: 12.99,
-      quantity: 500
+      quantity: 500,
     },
     {
-      name: 'Macarons',
+      name: "Macarons",
       description:
-        'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-      image: 'macarons.jpg',
+        "This elegant treat comes in a variety pack of ten different macarons.",
+      image: "macarons.jpg",
       category: categories[0]._id,
       price: 8.99,
-      quantity: 500
+      quantity: 500,
     },
     {
-      name: 'Plates',
+      name: "Plates",
       category: categories[1]._id,
       description:
-        'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-      image: 'plates.jpg',
+        "These plates were hand created in Italy and are microwave and dishwasher safe.",
+      image: "plates.jpg",
       price: 17.99,
-      quantity: 20
+      quantity: 20,
     },
     {
-      name: 'Laptop',
+      name: "Laptop",
       category: categories[2]._id,
       description:
-        'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-      image: 'laptop.jpg',
+        "This is the new 13.3 inch model NAC7 laptop created by our very own here at list-hub.",
+      image: "laptop.jpg",
       price: 899.99,
-      quantity: 50
+      quantity: 50,
     },
     {
-      name: 'Television',
+      name: "Television",
       category: categories[2]._id,
-      description:
-        'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-      image: 'television.jpg',
+      description: "This is a brand new 62 inch flat screen television.",
+      image: "television.jpg",
       price: 749.99,
-      quantity: 100
+      quantity: 100,
     },
     {
-      name: 'Robot Vacuum',
+      name: "Robot Vacuum",
       category: categories[2]._id,
       description:
-        'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-      image: 'robot-vac.jpg',
+        "Ever too tired to vacuum? This here lil robot will do it for you.",
+      image: "robot-vac.jpg",
       price: 599.99,
-      quantity: 30
+      quantity: 30,
     },
     {
-      name: 'The Passion Within',
+      name: "The Passion Within",
       category: categories[3]._id,
-      description:
-        'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
-      image: 'passion-within-book.jpg',
+      description: "The Passion Within -- A Women's journal",
+      image: "passion-within-book.jpg",
       price: 10.99,
-      quantity: 30
+      quantity: 30,
     },
     {
-      name: 'The World Atlas of Coffee',
+      name: "The World Atlas of Coffee",
       category: categories[3]._id,
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
-      image: 'coffee-book.jpg',
+        "This is the perfect book for every coffee connoisseur out there.",
+      image: "coffee-book.jpg",
       price: 19.99,
-      quantity: 100
+      quantity: 100,
     },
     {
-      name: 'Your Soul is a River',
+      name: "Your Soul is a River",
       category: categories[4]._id,
-      description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-      image: 'soul-book.jpg',
+      description: "Your Soul is a River by Nikita Gill",
+      image: "soul-book.jpg",
       price: 14.99,
-      quantity: 100
+      quantity: 100,
     },
     {
-      name: 'Set of Toy Soldiers',
+      name: "Set of Toy Soldiers",
       category: categories[4]._id,
-      description:
-        'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
-      image: 'soldiers-toy.jpg',
+      description: "A 7-pack of miniature toy soldiers.",
+      image: "soldiers-toy.jpg",
       price: 2.99,
-      quantity: 1000
+      quantity: 1000,
     },
     {
-      name: 'Unicorn Plush',
+      name: "Unicorn Plush",
       category: categories[4]._id,
-      description:
-        'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-      image: 'unicorn-toy.jpg',
+      description: "A plush stuffed unicorn toy for children.",
+      image: "unicorn-toy.jpg",
       price: 7.99,
-      quantity: 100
+      quantity: 100,
     },
     {
-      name: 'Set of Dinosaurs',
+      name: "Set of Dinosaurs",
       category: categories[4]._id,
-      description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
-      image: 'dinosaur-toy.jpg',
+      description: "Features a pack of assorted dinosaurs.",
+      image: "dinosaur-toy.jpg",
       price: 9.99,
-      quantity: 600
-    }
+      quantity: 600,
+    },
   ]);
 
-  console.log('products seeded');
+  console.log("products seeded");
 
   await User.deleteMany();
 
   await User.create({
-    userName: 'Pamela',
-    email: 'pamela@testmail.com',
-    password: 'password12345',
+    userName: "Pamela",
+    email: "pamela@testmail.com",
+    password: "password12345",
     orders: [
       {
-        products: [products[0]._id, products[0]._id, products[1]._id]
-      }
-    ]
+        products: [products[0]._id, products[0]._id, products[1]._id],
+      },
+    ],
   });
 
   await User.create({
-    userName: 'Elijah',
-    email: 'eholt@testmail.com',
-    password: 'password12345'
+    userName: "Elijah",
+    email: "eholt@testmail.com",
+    password: "password12345",
   });
 
-  console.log('users seeded');
+  console.log("users seeded");
 
   process.exit();
 });
